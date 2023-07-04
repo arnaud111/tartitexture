e5e286b90e3e2724f5be8b1ac9d70160ad213a15
Arnaud <arnaudjourdain@orange.fr>
2023-07-04 19:46:34 +0200

    init all modules

---
+++ file/src/test/java/esgi/tartitecture/file/FileApplicationTests.java
@@ -0,0 +1,13 @@
+package esgi.tartitecture.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
